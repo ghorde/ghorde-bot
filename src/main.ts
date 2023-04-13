fb35a422ca33f538c1098f99764aff8232fd5c63
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-13 07:11:49 +0530

    feat: bot works

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-console.log("hello world")
-
 import * as dotenv from 'dotenv'
 import { Client } from 'guilded.js'
 
 dotenv.config()
 
-console.log(process.env.LOL)
+const TOKEN = process.env.GUILDED_TOKEN
+
+const client = new Client({token: TOKEN})
+
+client.on("ready", () => console.log(`Bot is successfully logged in`));
+	client.on("messageCreated", (message) => {
+			if (message.content === "test") {
+				    return message.reply("test indeed");
+			}
+	});
+
+client.login();
