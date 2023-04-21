0cd52730178b0e4fdaf814f56d63b95ffcbabaf2
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-21 20:48:23 +0530

    fix: added missing lm command

---
+++ src/commands/diffusion.command/list-models.command.ts
@@ -0,0 +1,42 @@
+import { axios, mainLogger } from '../../main';
+import { CommandGeneric } from '../../factories/command/command.factory';
+import { ErrorEmbed, SuccessEmbed, WarningEmbed } from '../../helpers/embeds';
+import { AxiosResponse } from 'axios';
+import { getFlags, removeFlags } from 'helpers/common/flag-detector';
+import { paginate } from 'helpers/common/paginate';
+
+interface IModelDetails {
+    performance: number
+    queued: number
+    eta: number
+    type: string
+    name: string
+    count: number
+}
+
+export const listModels = new CommandGeneric('listModels', ['lm'], 'Lists models in the horde!', 'lm <page number>', 'DIFFUSION', async(client, message, args) => {
+    if (parseInt(args[0])) {
+        const availableModels = await axios.get('sh/models').catch((err) => {
+            mainLogger.error(err)
+            const response = ErrorEmbed(client, message).setTitle('🎨 Diffusion!').setDescription(`Unable to get list of models ;-;`);
+            message.reply(response)
+            return
+        })
+        if (!availableModels) {
+            return
+        }
+        const commandsPage = paginate<IModelDetails>(availableModels.data, parseInt(args[0]), 5);
+        if (commandsPage) {
+            message.reply(SuccessEmbed(client, message).setFooter(`Viewing ${commandsPage.page}/${commandsPage.total_pages}`).setTitle("Help").addFields(commandsPage.data.map(model => {
+                return {
+                    name: `${model.name} - ${model.type}`,
+                    value: `Performance: ${model.performance}\nQueued: ${model.queued}\nETA: ${model.eta}\nCount: ${model.count}`,
+                    inline: true
+                }
+            })));
+            return;
+        }
+    }
+    message.reply(ErrorEmbed(client, message).setDescription("Provide Page Number"))
+    return;
+});
