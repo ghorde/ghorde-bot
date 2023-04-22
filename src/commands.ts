1b3ec3bce5ad566db2f7a45aac9b5660dcfb85af
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-23 00:19:48 +0530

    fix: removed random image command

--- src/commands.ts
+++ src/commands.ts
@@ -2,14 +2,13 @@ import { ICommand, botCommand } from "./factories/command/command.factory.types"
 import { ping } from './commands/ping.command';
 import { help } from './commands/help.command';
 import { prefix } from "./commands/prefix.command";
-import { randomImage } from './commands/randomImage.command';
 import { waifu } from './commands/waifu.command';
 import { edit } from "./commands/edit.command";
 import { diffusion } from './commands/diffusion.command';
 import { listModels } from './commands/diffusion.command/list-models.command';
 import { modelStats } from './commands/diffusion.command/model-stats.command';
 
-export const commands: Record<string, ICommand> = {ping, help, prefix, randomImage, waifu, edit, diffusion, listModels, modelStats}
+export const commands: Record<string, ICommand> = {ping, help, prefix, waifu, edit, diffusion, listModels, modelStats}
 
 export const aliasUnique = (commands: Record<string, ICommand>): boolean => {
     const aliases = Object.values(commands).map(command => command.aliases).flat();
